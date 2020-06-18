259aed610394837b749cd0e79200719ea2b6181b
nikos chalikias <nikos.chalikias@gmail.com>
2020-06-18 20:50:20 +0100

    Created in nBlocks Studio server and in nBlocksStudio github by nikoschalikias, author Ian Aherne

---
+++ neopixel.cpp
@@ -0,0 +1,64 @@
+#include "neopixel.h"
+
+
+
+nBlock_NeoPixel::nBlock_NeoPixel(PinName MOSI):_neopixel(MOSI){
+	
+	greenFlag = 0; // initialise all flags to zero
+	redFlag = 0;
+	blueFlag = 0;
+	numberFlag = 0;
+	
+	
+	
+}
+
+/**
+* @brief    triggerInput()
+* @details  When any of the inputs are triggered the the colour variable
+*			for the corresponding input number will be updated and its flag
+*			raised to show it has been updated.
+* @param    Number of the input, value of the input
+* @return   NA.
+* 
+*/
+
+
+void nBlock_NeoPixel::triggerInput(uint32_t inputNumber, uint32_t value){
+	
+	
+	
+	if (inputNumber == 0){green = (uint8_t)value;greenFlag = 1;}
+	else if(inputNumber == 1){red = (uint8_t)value;redFlag = 1;}
+	else if(inputNumber == 2){blue = (uint8_t)value;blueFlag = 1;}
+	else if(inputNumber == 3){number = (uint8_t)value; numberFlag = 1;}	
+	
+}
+
+/**
+* @brief    endFrame()
+* @details  Once all the flags are raised a function call to the LED library is executed and the accumulated data is sent.
+*			The flags prevent a function call every time an input changes which would be incorrect behaviour.
+* @param    NA.
+* @return   NA.
+* 
+*/
+
+void nBlock_NeoPixel :: endFrame(void){
+	
+	if (greenFlag == 1 && redFlag == 1 && blueFlag == 1 && numberFlag == 1){
+		
+		_neopixel.send_colour(green,red,blue,number);
+		
+		greenFlag = 0;
+		redFlag = 0;
+		blueFlag = 0;
+		numberFlag = 0;
+	
+	}
+	
+	
+	
+}
+
+
